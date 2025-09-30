0a9625a27ce504ad098f8ee4729d066298e1e802
MarinFotache <marinfotache@gmail.com>
2025-09-30 10:07:34 +0300

    Update 3b ML_1NF_3NF_scoring_log10_duration.R

--- scripts/3b ML_1NF_3NF_scoring_log10_duration.R
+++ scripts/3b ML_1NF_3NF_scoring_log10_duration.R
@@ -28,9 +28,9 @@ library(ggsci)
 library(svglite)
 library(corrr)
 
-
-
-# uncomment and execute the next line to set the default/current directory according to your local confoguration
+# By default, the current directory is associated to the folder when cloning
+#  the repo from github.
+# Uncomment and execute the next line if you want to change the default/current directory,
 # setwd('here is the path to your current directory')
 
 base_path <- getwd()
